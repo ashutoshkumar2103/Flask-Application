64a71bc2886acb297d01e044b804bfbe1eb6b89d
Ashutosh Kumar <ashutoshkr0312@gmail.com>
2025-07-27 12:58:14 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,45 +1,37 @@
-# # Importing Flask to create a web application
+# importing necessary libraries 
 
-from flask import Flask,render_template, request
-from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+import datetime
 import requests
 
-# Backend URL for API calls
-BACKEND_URL = 'http://127.0.0.1:9000'
+BACKEND_URL = 'http://127.0.0.1:2000'
 
-# # Initializing the Flask application
 app = Flask(__name__)
 
-# # Defining a route for the home page
 @app.route('/')
-# # Function to handle requests to the home page
 def home():
-    day_of_the_week = datetime.now().strftime("%A")
-    print("Today is " + day_of_the_week)
+    day_of_the_week = datetime.datetime.now().strftime('%A')
+    print("Today is:", day_of_the_week)
+
     return render_template('index.html', dayOfTheWeek=day_of_the_week)
 
-
+# Submitting data to the backend
 @app.route('/submit', methods=['POST'])
 def submit():
-    # Collecting form data from the request
     form_data = dict(request.form)
-    
-    # Sending a POST request to the backend to submit the data
     requests.post(BACKEND_URL + '/submit', json=form_data)
-    
-    # Returning the response from the backend
-    return 'Data submitted successfully!'
+    return redirect(url_for('success'))
 
+# Success route to handle redirection after form submission
+@app.route('/success')
+def success():
+    return ("Data submitted successfully!")
+
+# Fetching data from the backend to display on the frontend
 @app.route('/data')
 def data():
-    # Fetching data from the backend
     response = requests.get(BACKEND_URL + '/data')
-    
-    # Returning the data fetched from the backend
     return response.json()
 
-
-# # This allows the app to be run with the command `python app.py` and it will start the Flask development server
 if __name__ == '__main__':
-#     # Running the Flask application
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host= '0.0.0.0', port=1000, debug=True)
